da890455720e864d9c70c7ee5f5b406710a4797e
Shehab Ahmed <just.shiba3006@gmail.com>
2024-02-20 08:29:25 +0200

    get http package and created get all products service.

--- lib/services/get_all_products_service.dart
+++ lib/services/get_all_products_service.dart
@@ -1,3 +1,17 @@
-class  GetallProductsService {
- 
+import 'dart:convert';
+
+import 'package:store_app/models/product_model.dart';
+import 'package:http/http.dart' as http;
+
+class GetallProductsService {
+  Future<List<ProductModel>> getAllProducts() async {
+    http.Response response =
+        await http.get(Uri.parse('https://fakestoreapi.com/products'));
+    List<ProductModel> productsList = [];
+    List<dynamic> data = jsonDecode(response.body);
+    for (var element in data) {
+      productsList.add(ProductModel.fromJson(element));
+    }
+    return productsList;
+  }
 }
